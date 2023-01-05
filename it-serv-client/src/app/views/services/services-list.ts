7f4645dc41dc8c142f50057e44509dd602b02b06
Ryan_PrismSoftware <58835537+VanillaHoward@users.noreply.github.com>
2023-01-05 14:00:45 +1000

    Text updates

--- it-serv-client/src/app/views/services/services-list.ts
+++ it-serv-client/src/app/views/services/services-list.ts
@@ -8,37 +8,37 @@ const services: Service[] = [
   {
     title: 'Microsoft 365 Migration Specialists',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Let us migrate your services and devices to Microsoft 365 with ease of mind and minimal downtime',
     icon: 'fa-brands fa-microsoft fa-5x',
   },
   {
     title: 'Mail Management',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Let us manage your organisations mail with unlimited requests as a part of a package',
     icon: 'fa-solid fa-envelope-open-text fa-5x',
   },
   {
     title: 'Managed Services',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Let us tailor a package to suit your organisation. Sit back whilst we manage your IT services, devices, network and infrastructure',
     icon: 'fa-solid fa-house-laptop fa-5x',
   },
   {
     title: 'Office 365 Suite',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Can be purchased stand-alone or tailored with your office 365 tenancy. Rest assured, enquire within, and let us handle the stress for you.',
     icon: 'fa-solid fa-file-word fa-5x',
   },
   {
     title: 'Web Hosting',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Professional and reliable web hosting services with minimal downtime. In house developers for special requests of implementation',
     icon: 'fa-solid fa-server fa-5x',
   },
   {
     title: 'IT Audits',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellat architecto esse iure est, fugiat quo distinctio quidem provident ad.',
+      'Let us audit your IT infrastructure to identify and assist with security risks, vulnerabilities, device management, network configurations and business continuity',
     icon: 'fa-solid fa-magnifying-glass-chart fa-5x',
   },
 ];
